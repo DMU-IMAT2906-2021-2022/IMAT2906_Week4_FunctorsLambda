853529803f93816ff2af156695b189355d5e0978
jethros <jethros@dmu.ac.uk>
2022-02-02 10:53:50 +0000

    Initial commit

---
+++ FunctAndLamb.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+//!
+//!A simple print string function that returns no value.
+//!
+void GameName() {
+	std::cout << "Killer Cats From Mars!" << std::endl;
+}
+
+
+int main() {
+	
+	system("pause");
+	return 0;
+}
